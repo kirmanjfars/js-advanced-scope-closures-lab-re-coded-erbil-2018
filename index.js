61096a9fe78ce68e19d4f929447a08cfd88a920d
Kirmanj Faris <kirmanj.00806510@gmail.com>
2018-08-22 12:38:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 function produceDrivingRange(blockRange ){
   return function(start, end){
-    let temp1= parseInt(start);
-    let temp2 = parseInt(end);
-    let temp3 = temp2 - temp2;
-    let result = blockRange - temp3;
-    
-    if(result > 0){
-       return `within range by ${result}`
+   let start = parseInt(startingBlock);
+    let end = parseInt(endingBlock);
+    let distanceToTravel = Math.abs(end - start);
+    let difference = blockRange - distanceToTravel;
+
+    if( difference > 0){
+      return `within range by ${difference}`
     } else {
-      return `${Math.abs(result)} blocks out of range`
+      return `${Math.abs(difference)} blocks out of range`
     }
-    
   }
 }
