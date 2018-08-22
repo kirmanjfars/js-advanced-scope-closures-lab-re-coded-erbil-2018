054030839c944074e44c078619ce191eeee6d4b9
Kirmanj Faris <kirmanj.00806510@gmail.com>
2018-08-22 12:40:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ function produceDrivingRange(blockRange ){
 }
 
 
-function produceTipCalculator(){
-  return function (){
-    
+function produceTipCalculator(x){
+  return function (y){
+    return x*y;
   }
 }
