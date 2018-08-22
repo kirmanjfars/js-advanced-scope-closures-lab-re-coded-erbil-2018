7349177c3c13516542b2cdd56b0ace91fcefcec6
Kirmanj Faris <kirmanj.00806510@gmail.com>
2018-08-22 12:34:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ function produceDrivingRange(blockRange ){
   return function(start, end){
     let temp1= parseInt(start);
     let temp2 = parseInt(end);
-    let result = temp2 - temp2;
+    let temp3 = temp2 - temp2;
+    let result = blockRange - temp3;
     
     if(result > 4){
       return '2 blocks out of range';
